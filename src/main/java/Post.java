77e3fc9b754d1abc3f494f35a1e9cd36036b65bd
Fedor Shakrun <fshakrun@gmail.com>
2022-10-01 11:55:43 +0300

    classes added

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+
+    public boolean subscription = true;
+
+    public FormDate birthday;
+
+}
